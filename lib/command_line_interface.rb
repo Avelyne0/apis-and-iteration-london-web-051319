7f957b8439f70fddfdaad9ead4efbb8c3c0f62dc
Avelyne0 <sam.dbarrie@gmail.com>
2019-05-14 11:27:37 +0100

    changed the input method

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -3,9 +3,9 @@ def welcome
 puts "Welcome, to the party!"
 end
 
-def get_character_from_user(user_input)
-  puts "please enter a character name"
-  gets.chomp.user_input
+def get_user_input
+  puts "Please enter a search term"
+  gets.chomp
   binding.pry
   # use gets to capture the user's input. This method should return that input, downcased.
 end
