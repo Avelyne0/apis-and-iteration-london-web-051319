464e71d84fdc98a3eae3f3f10436d21c16b2e602
Avelyne0 <sam.dbarrie@gmail.com>
2019-05-14 11:24:14 +0100

    bad start

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
+require "pry"
 def welcome
-  # puts out a welcome message here!
+puts "Welcome, to the party!"
 end
 
-def get_character_from_user
+def get_character_from_user(user_input)
   puts "please enter a character name"
+  gets.chomp.user_input
+  binding.pry
   # use gets to capture the user's input. This method should return that input, downcased.
 end
